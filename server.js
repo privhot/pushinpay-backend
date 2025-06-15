125ebabf97641b4e0d8d4728e0807dac409e2796
Felipe <felipe.orangel949@gmail.com>
2025-06-15 03:50:21 -0300

    Adiciona rota / para resposta simples

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.get("/payment-status/:hash", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Erro interno no servidor" });
-  }
+ app.get('/', (req, res) => {
+  res.send('API PushinPay funcionando!');
 });
 
 app.listen(PORT, () => {
